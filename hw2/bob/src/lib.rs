31fa3ba2e58314474ab6bd524405e66c676752bf
steve3p0 <stevebpdx@gmail.com>
2018-08-01 02:51:49 -0700

    Done!

--- hw2/bob/src/lib.rs
+++ hw2/bob/src/lib.rs
@@ -3,94 +3,51 @@ use regex::Regex;
 
 pub fn reply(message: &str) -> &str
 {
+    let r_special = r"[`~!@#$%&*()_=+{}|;:',.<>/?]";
+    let r_special_esc = r"[\^\[\]\-\\]";
+    let r_1or_more_exc = r"(!+)";
+    let r_0or_more_exc = r"(!*)";
+
     let msg = message.trim();
 
     // Just look at Bob weird
     if msg == ""
     { return "Fine. Be that way!"}
 
-    // Yell Ask
-    //^([A-Z]+\s*)+(\?+)$
-    //^(([0-9A-Z]|[^\P{P}\?])+\s*)+(\?+)$
-    let mut re = Regex::new(r"^([A-Z]+\s*)+(\?+)$").unwrap();
-    if re.is_match(msg)
-    { return "Calm down, I know what I'm doing!" }
-
-
-    // Question Mark
+    // Yell: CAPS Ask
+    {
+        let r = r"^([A-Z]+\s*)+(\?+)$";
+        if bob_match(&r, msg)
+        { return "Calm down, I know what I'm doing!" }
+    }
+    // Ask: Question Mark
     if msg.ends_with("?")
     { return "Sure." }
 
-    // Yelling
-    // ^([A-Z]+\s*)+(\!*)$
-    // (([0-9A-Z]|[^\P{P}\?])+\s*)+(!*)
-    //^(([0-9A-Z]|[^\P{P}\?])+\s*)+(!*))$
-
-
-
-    // Yelling: numbers, punct, and !
+    // Yell: CAPS, numbers, punct, and !
     {
-        //let r_special = r"[%\^*@#$()\[\]=+_\-`'&\\/{}|,?!<>~;:.]";
-        let r_special = r"[%*@#$()=+_`'&/{}|,?!<>~;:.]";
-        let r_special_esc = r"[\^\[\]\-\\]";
+        let r_caps_nums_spec = format!(r"(([0-9A-Z]|{}|{})+\s*)+", r_special, r_special_esc);
+        let r = format!(r"^{}{}$", &r_caps_nums_spec, r_1or_more_exc);
 
-        let r_1or_exc = r"(!+)";
-        //let r_nums_punc = r"(([0-9A-Z]|[%\^*@#$()\[\]=+_\-`'&\\/{}|,?!<>~;:.])+\s*)+";
-        //let r_nums_punc = format!(r"(([0-9A-Z]|{})+\s*)+", r_special);
-        let r_nums_punc = format!(r"(([0-9A-Z]|{}|{})+\s*)+", r_special, r_special_esc);
-
-        let r = format!(r"^{}{}$", &r_nums_punc, r_1or_exc);
-        //let r = format!(r"^({}{})|({}{})$", r_yell, r_0or_exc, r_yell, r_1or_exc);
-
-        re = Regex::new(&r).unwrap();
-        if bob_match(&r, msg)
-        { return "Whoa, chill out!" }
-    }
-//
-//    re = Regex::new(&r).unwrap();
-//    if re.is_match(msg)
-//        { return "Whatever." }
-//
-//    let r_0or_exc = r"(!*)";
-//    let r_1or_exc = r"(!+)";
-
-    //let r_yell = r"(([0-9A-Z]|[^\P{P}\?])+\s*)+";
-    // .^$*+?()[{\|
-
-    //%^*@#$(^
-    //"
-    //let r_spec = r"%\^\*@#\$\(";
-
-    let r_spec = r"%";
-    //\^\*@#\$\(]
-    //let r_spec = r"!@#\$%\^&\*\(\)_+\-=\[\]{};':\\\|,.<>\/\?";
-    let r_yell = r"(([A-Z]|[%\^])+\s*)+";
-    let r_0or_exc = r"(!*)";
-    //let r_1or_exc = r"(!+)";
-
-    {
-        let r = format!(r"^{}{}$", r_yell, r_0or_exc);
-        //let r = format!(r"^({}{})|({}{})$", r_yell, r_0or_exc, r_yell, r_1or_exc);
-
-        re = Regex::new(&r).unwrap();
         if bob_match(&r, msg)
         { return "Whoa, chill out!" }
     }
 
+    // Yell: CAPS and 0 or more !
+    {
+        let r_caps_spec = format!(r"(([A-Z]|{}|{})+\s*)+", r_special, r_special_esc);
+        let r = format!(r"^{}{}$", r_caps_spec, r_0or_more_exc);
+
+        if bob_match(&r, msg)
+        { return "Whoa, chill out!" }
+    }
 
     "Whatever."
-
-
-
-
-
-
-    //unimplemented!("have Bob reply to the incoming message: {}", message)
 }
 
 fn bob_match(r: &str, msg: &str) -> bool
 {
-    println!("r: {}", r);
+    //println!("r: {}", r);
 
     let re = Regex::new(r).unwrap();
     return  re.is_match(msg)
