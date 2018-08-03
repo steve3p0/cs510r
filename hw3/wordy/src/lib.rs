e6a7cb730732e26f6db8e12ec034e3dd60c24354
steve3p0 <stevebpdx@gmail.com>
2018-08-03 01:16:42 -0700

    regex is working.

--- hw3/wordy/src/lib.rs
+++ hw3/wordy/src/lib.rs
@@ -8,296 +8,94 @@ struct Operation
     operator: String,
 }
 
+impl Operation
+{
+    fn new(operand: i32, operator: String) -> Self
+    {
+        Operation { operand: operand, operator }
+    }
+}
+
+//pub plus()
 
 pub fn answer(command: &str) -> i64
 {
-    0
-}
+    // Remove the word by - it's worthless in this context
+    let s = command.replace(" by", "");
 
-pub fn simple_answer(command: &str) -> i64
-{
     // Vector for getting the Operations: operator operand
     let mut operations: Vec<Operation> = Vec::new();
 
-    let r = r"What\sis\s(?:([0-9]+))";
-    let re = Regex::new(r).unwrap();
-    let caps = re.captures(command).unwrap();
-    let op1 = caps.get(1).unwrap().as_str();
-    assert_eq!(op1, "5");
+    // Get first operand
 
-    let operator = "plus".to_string();
-    let operand = op1.parse::<i32>().unwrap();
-    let operation1 = Operation { operand: operand, operator: operator };
+    // What\sis\s(?:([0-9]+))
+    // What\sis\s(?:\d+)
+    //let pattern = r"What\sis\s(?:\-?\d+)";
+    //let pattern = r"What\sis\s(?:([0-9]+))";
+    //let pattern = r"What\sis\s(?:(\d+))";
+    let pattern = r"What\sis\s(?:(\-?\d+))";
 
-    //println!("operation1: {}", m.as_str());
-    println!("operator1: {}", operation1.operator);
-    println!("operand1: {}", operation1.operand);
-    println!();
-
-    operations.push(operation1 );
+    let operation = get_operand(pattern, &s);
+    operations.push(operation );
 
     // Now get the operands
-    //let pattern = r"\d+";
-    let pattern = r"plus\s\d+";
-    let regex= match Regex::new(pattern)
+    //(plus|minus|multiplied\sby|divided\sby)
+    let pattern = r"(plus|minus|multiplied|divided)\s\-?\d+";
+    operations = get_operations(operations, pattern, &s);
+
+    0
+}
+
+fn get_operand(pattern: &str, s: &str) -> Operation
+{
+    let re= match Regex::new(pattern)
     {
         Ok(r) => r,
         Err(e) => { panic!("Could not compile regex: {}", e); }
     };
 
-    let matches = regex.find_iter(command);
+    let caps = re.captures(s).unwrap();
+    let op1 = caps.get(1).unwrap().as_str();
+
+    // Default 1st operand to 'plus'
+    let operand = op1.parse::<i32>().unwrap();
+    let operator = "plus".to_string();
+    let operation = Operation::new(operand, operator);
+
+    //println!("operation1: {}", m.as_str());
+    println!("operator1: {}", operation.operator);
+    println!("operand1: {}", operation.operand);
+    println!();
+
+    operation
+}
+
+fn get_operations(mut operations: Vec<Operation>, pattern: &str, s: &str) -> Vec<Operation>
+{
+    let re= match Regex::new(pattern)
+    {
+        Ok(r) => r,
+        Err(e) => { panic!("Could not compile regex: {}", e); }
+    };
+
+    let matches = re.find_iter(s);
 
     for m in matches
     {
         let mut s = m.as_str().split_whitespace();
         let operator = s.next().unwrap().to_string();
         let operand = s.next().unwrap().parse::<i32>().unwrap();
-        let operation = Operation { operand: operand, operator: operator };
+        let operation = Operation::new(operand, operator);
 
         println!("operation: {}", m.as_str());
-        println!("operator: {}", operation.operator);
         println!("operand: {}", operation.operand);
+        println!("operator: {}", operation.operator);
         println!();
 
         operations.push(operation );
     }
 
-    0
+    operations
 }
 
 
-//    let m = re.find(command).unwrap();
-//let caps = re.captures(command).unwrap();
-//let text1 = caps.get(1).map_or("", |m| m.as_str());
-//let text2 = caps.get(2).map_or("", |m| m.as_str());
-//assert_eq!(text1, "5");
-//assert_eq!(text2, "");
-
-
-//fn get_matches(pattern: &str, s: &str) -> Matches
-//{
-//    let regex = match Regex::new(pattern)
-//    {
-//        Ok(r) => r,
-//        Err(e) => { panic!("Could not compile regex: {}", e); }
-//    };
-//
-//    let operators = regex.find_iter(s);
-//
-//    for o in operators
-//    {
-//        println!("operator: {}", o.as_str());
-//    }
-//}
-
-
-pub fn simple_answer2(command: &str) -> i64
-{
-
-//    // get first operand
-//    let r = r"(?:What\sis\s)([0-9]+)";
-//    let re = Regex::new(r).unwrap();
-//    let operand1 = match re
-//    {
-//        _ => {}
-//    }
-//
-//    let caps = re.captures(command).unwrap();
-//    let operand1:i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-//
-//    println!("operand1: {}", operand1);
-
-//    // Now for the rest
-//    let pat = r"/\d+/g";
-//    let re = Regex::new(pat).unwrap();
-//
-//    //let caps = re.captures(command).iter();
-//    //let caps1 = re.captures(command);
-//    let caps2 = re.captures(command).unwrap();
-//
-//    for c in caps2.iter()
-//    {
-//        let cp = c.unwrap().as_str();
-//        println!("c: {}", cp);
-//    }
-
-    //
-//    let pattern = r"plus|minus|multiplied\sby|divided\sby)";
-//    let regex = match Regex::new(pattern)
-//    {
-//        Ok(r) => r,
-//        Err(e) => { panic!("Could not compile regex: {}", e); }
-//    };
-//
-//    let result = regex.find_iter(command);
-//
-//    //for (start, end) in result
-//    for m in result
-//    {
-//        println!("m: {}", m.as_str());
-//    }
-//
-
-
-    //(\s(plus|minus|multiplied\sby|divided\sby)\s[0-9]+)+
-    // ([0-9])(\s(plus)\s[0-9]+)+
-
-    // (?:\s)(plus)(?:\s)([0-9]+)+
-    // (?:(?:\s)(plus)(?:\s)([0-9]+))+
-    // ((?:\s)(plus)(?:\s)([0-9]+)+)
-
-    //let r1 = r"(\s(plus|minus|multiplied\sby|divided\sby)\s[0-9]+)+";
-    //let r1 = r"(?:(?:\s)(plus)(?:\s)([0-9]+))+";
-    //let r1 = r"(?:\s)(plus)(?:\s)([0-9]+)+";
-    //let r1 = r"";
-    //let r1 = r"((\s)(plus)(\s)([0-9]+)+)";
-    //let r1 = r"(plus\s[0-9]+)+";
-    //let r1 = r"(plus\s[0-9]+)+";
-    //let r1 = r"(plus\s\d+)+";
-
-
-//    let r1 = r"\d+";
-//    let re = Regex::new(r1).unwrap();
-//
-//    //let caps = re.captures(command).iter();
-//    let caps1 = re.captures(command);
-//    let caps2 = re.captures(command).unwrap();
-//
-//    for c in caps2.iter()
-//    {
-//        let cp = c.unwrap().as_str();
-//        println!("c: {}", cp);
-//    }
-
-//    let cap1 = caps.get(1).map_or("", |m| m.as_str());
-//    let cap2 = caps.get(2).map_or("", |m| m.as_str());
-//
-//    println!("cap 1: {}", cap1);
-//    println!("cap 2: {}", cap2);
-
-
-
-//
-//    let operand1 = re.captures(command).unwrap().get(1).unwrap().as_str();
-//
-//    let cap
-
-    //captures(command).unwrap().get(1).unwrap().as_str();
-    //captures.get(1).unwrap().as_str()
-
-
-//    {
-//        //Some(caps) => println!("Found match: {}", caps.get(0).unwrap().as_str()),
-//        //Some(caps) => println!("Found match: {}", &caps[0]),
-//        &caps[0],
-//        //Some(caps) => &caps[0].borrow(),
-//        None => panic!("Can't find operand")
-//    };
-
-    //println!("operand1: {}", operand1);
-
-
-//    // ([0-9])(\s(plus)\s[0-9]+)+
-//    let r = r"([0-9])(\s(plus)\s[0-9]+)+";
-//
-//    let re = Regex::new(r).unwrap();
-//
-//    let captures = re.captures_iter(command).collect::<Vec<_>>();
-//    let tokens: Vec<(&str, &str)> = captures.iter()
-//        .flat_map(|t| t.iter_named())
-//        .filter(|t| t.1.is_some())
-//        .map(|t| (t.0, t.1.unwrap()))
-//        .collect();
-//
-//    for token in tokens
-//    {
-//        println!("{:?}", token);
-//    }
-
-    //let caps = re.captures(com).iter();
-//    let caps = re.captures(com).unwrap();
-//
-//    let cap1 = caps.get(1).map_or("", |m| m.as_str());
-//    let cap2 = caps.get(2).map_or("", |m| m.as_str());
-//
-//    println!("cap 1: {}", cap1);
-//    println!("cap 2: {}", cap2);
-
-//    for c in caps
-//    {
-//        c.
-//    }
-//
-//    if  re.is_match(command)
-
-    0
-}
-
-pub fn simple_answer1(command: &str) -> i64
-{
-
-    let com = "5 plus 13 plus 6";
-
-    // ([0-9])(\s(plus)\s[0-9]+)+
-    let r = r"([0-9])(\s(plus)\s[0-9]+)+";
-
-    let re = Regex::new(r).unwrap();
-    //let caps = re.captures(com).iter();
-    let caps = re.captures(com).unwrap();
-
-    let cap1 = caps.get(1).map_or("", |m| m.as_str());
-    let cap2 = caps.get(2).map_or("", |m| m.as_str());
-
-    println!("cap 1: {}", cap1);
-    println!("cap 2: {}", cap2);
-
-//    for c in caps
-//    {
-//        c.
-//    }
-//
-//    if  re.is_match(command)
-
-    0
-}
-
-
-
-/// Calculates the Sum
-/// Takes the sum of n and m
-/// Notes: What asserts are necessary here?
-///    - Zero and Negative are valid, correct?
-pub fn sum (n: i64, m: i64) -> i64
-{
-    n + m
-}
-
-
-/// Calculates the Product
-/// Takes the product of n and m
-/// Notes: What asserts are necessary here?
-///    - Zero and Negative are valid, correct?
-pub fn product (n: i64, m: i64) -> i64
-{
-    n * m
-}
-
-
-/// Calculates the Greatest Common Divisor
-/// Taken directly from Programming Rust by Jim Blandy, Jason Orendorff
-/// Chapter 2, Handling Command-Line Arguments (pg 10) with slight
-/// modifications:
-///    - (params are i64 instead of u64)  - a common interface for all operations
-///    - we change the assert to make no zero or negative values
-pub fn gcd(mut n: i64, mut m: i64) -> i64 {
-    assert!(n > 0 && m > 0);
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
-        }
-        m = m % n;
-    }
-    n
-}
--- hw3/wordy/tests/wordy_steve.rs
+++ hw3/wordy/tests/wordy_steve.rs
@@ -1,15 +1,52 @@
 extern crate wordy;
 
-use wordy::*;
+//use wordy::*;
 
 #[test]
-fn simple_answer() {
-    let command = "What is 5 plus 13 plus 6 plus 15?";
-    assert_eq!(39, wordy::simple_answer(command));
+fn simple_answer_plus1() {
+    let command = "What is -5 plus 13?";
+    assert_eq!(39, wordy::answer(command));
 }
 
 #[test]
-fn simple_answer1() {
-    let command = "What is 5 plus 13 plus 6?";
-    assert_eq!(24, wordy::simple_answer(command));
+fn simple_answer_plus2() {
+    let command = "What is 5 plus 13 plus -6?";
+    assert_eq!(39, wordy::answer(command));
 }
+
+#[test]
+fn simple_answer_plus3() {
+    let command = "What is 5 plus 13 plus -6 plus -15?";
+    assert_eq!(39, wordy::answer(command));
+}
+
+#[test]
+fn simple_answer_minus() {
+    let command = "What is 5 minus 4?";
+    assert_eq!(39, wordy::answer(command));
+}
+
+#[test]
+fn simple_answer_minus2() {
+    let command = "What is 10 minus 5 minus 2?";
+    assert_eq!(39, wordy::answer(command));
+}
+
+#[test]
+fn simple_answer_minus3() {
+    let command = "What is 10 minus 5 minus 2 minus 1?";
+    assert_eq!(39, wordy::answer(command));
+}
+
+#[test]
+fn simple_answer_multiply() {
+    let command = "What is 5 multiplied by 10?";
+    assert_eq!(39, wordy::answer(command));
+}
+
+#[test]
+fn simple_answer_divided() {
+    let command = "What is 10 divided by 5?";
+    assert_eq!(39, wordy::answer(command));
+}
+
