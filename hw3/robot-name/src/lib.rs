fe2b89d8c0aa81423cad608f984abdbc600c1124
steve3p0 <stevebpdx@gmail.com>
2018-08-05 23:52:33 -0700

    initial

---
+++ hw3/robot-name/src/lib.rs
@@ -0,0 +1,24 @@
+
+
+pub struct Robot
+{
+    name: String
+}
+
+impl Robot
+{
+    pub fn new() -> Robot
+    {
+        unimplemented!("Not implemented")
+    }
+
+    pub fn name<'a>(&'a self) -> &'a str
+    {
+        unimplemented!("Not implemented")
+    }
+    pub fn reset_name(&mut self)
+    {
+        unimplemented!("Not implemented")
+    }
+}
+
