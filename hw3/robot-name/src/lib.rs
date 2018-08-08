04d71658b9352fa788c3bafd8ebd7546f3333ff9
steve3p0 <stevebpdx@gmail.com>
2018-08-08 12:33:42 -0700

    changed to random number from range.

--- hw3/robot-name/src/lib.rs
+++ hw3/robot-name/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate time;
+extern crate rand;
 
-use std::collections::HashSet;
-use std::hash::Hash;
+use rand::Rng;
 
 pub struct Robot
 {
@@ -12,8 +12,6 @@ impl Robot
 {
     pub fn new() -> Robot
     {
-        check_duplicates(200000);
-
         let rnd_name = Robot::random();
         Robot { name: rnd_name }
     }
@@ -46,169 +44,130 @@ impl Robot
         // 10 x 10 x 10 = 1000
         // 647 x 1000 = 676,000
 
+        //let mut nano_u32:u32 = time::get_time().nsec as u32;
         let mut nano_u32:u32 = time::get_time().nsec as u32;
-        //println!("\nNanoseconds: {}", nano_u32);
+
+        let min = 0;
+        let max = 676000 - 1;
+
+        //let num = rand::thread_rng().gen_range(min, max);
+        let num = rand::thread_rng().gen_range(min, max) as u32;
+        println!("rand::thread_rng.gen_range({}, {}): {}", min, max, num);
+
+
+        println!("\nNanoseconds: {}", nano_u32);
+
 
         nano_u32 += 1;
-        let mut nano_string = right(nano_u32.to_string(), 6);
+        let mut nano_string = Robot::right(nano_u32.to_string(), 6);
         //println!("Nanoseconds as string: {}", nano_string);
 
-        let mut prefix_i32 = left(nano_string.clone(), 3).parse::<i32>().unwrap();
+        let mut prefix_i32 = Robot::left(nano_string.clone(), 3).parse::<i32>().unwrap();
         //println!("prefix_i32: {}", prefix_i32);
 
         if prefix_i32 > 675
-            {
-                prefix_i32 -= 400;
+        {
+            prefix_i32 -= 400;
 
-                nano_string = left(nano_string,3);
-                nano_string.push('0');
-            }
+            nano_string = Robot::left(nano_string,3);
+            nano_string.push('0');
+        }
 
-        let suffix = right(nano_string, 3);
-        let prefix_string = base26_alpha(prefix_i32);
+        let suffix = Robot::right(nano_string, 3);
+        let prefix_string = Robot::base26_alpha(prefix_i32);
         let random = format!("{}{}", prefix_string, suffix);
 
         random
     }
 
-}
-
-
-
-
-pub fn left(s: String, n: usize) -> String
-{
-    let len = s.len();
-    let mut s_right = s;
-
-    if len < n
+    pub fn base26_alpha(mut n: i32) -> String
     {
-        let mut dif = n - len;
-        while dif > 0
+        //let mut n = num; // 123;
+        let mut chars: Vec<char> = Vec::new();
+
+        loop
         {
-            s_right.push('0');
+            let (a, b) = Robot::divmod(n);
+            n = a;
 
-            dif += 1;
-        }
-    }
+            let c = Robot::itoa(b); // b + 65) as u8 as char;
 
-    s_right.chars().take(n).collect::<String>()
-}
+            chars.push(c);
 
-pub fn right(s: String, n: usize) -> String
-{
-    let len = s.len();
-    let mut s_right = s;
-
-    if len < n
-    {
-        let mut dif = n - len;
-        while dif > 0
-        {
-            s_right.push('0');
-
-            dif += 1;
-        }
-    }
-
-    let start = len - n;
-    s_right.chars().skip(start).take(len).collect::<String>()
-}
-
-pub fn base26_alpha(mut n: i32) -> String
-{
-    //let mut n = num; // 123;
-    let mut chars: Vec<char> = Vec::new();
-
-    loop
-    {
-        let (a, b) = divmod(n);
-        n = a;
-
-        let c = itoa(b); // b + 65) as u8 as char;
-
-        chars.push(c);
-
-        if n == 0 { break; }
-    }
-
-    if chars.len() < 2
-    {
-        chars.push('A');
-    }
-
-    chars.reverse();
-    let hash: String = chars.iter().collect();
-
-    //println!("int2ascii({}): {:?}", num, chars);
-
-    hash
-}
-
-pub fn itoa(n: i32) -> char
-{
-    let c = (n + 65) as u8 as char;
-
-    c
-}
-
-pub fn divmod(n: i32) -> (i32, i32)
-{
-    let mut a = n / 26;
-    let mut b= n % 26;
-
-    if b == 26
-    {
-        a = a - 1;
-        b = b + 26;
-    }
-
-    (a, b)
-}
-
-
-fn has_unique_elements<T>(iter: T) -> bool
-    where
-        T: IntoIterator,
-        T::Item: Eq + Hash,
-{
-    let mut uniq = HashSet::new();
-    let duplicate = iter.into_iter().all(move |x| uniq.insert(x));
-
-    println!("DUPLICATION: {}", duplicate );
-    duplicate
-}
-
-
-pub fn check_duplicates(n: u32)
-{
-    let mut hashes: Vec<String> = Vec::new();
-
-    let tenth = n / 10;
-    println!("1/10: {}", tenth);
-    //let tenth = (0.10 * n) as u32;
-
-    for i in 0..n
-    {
-        let rnd = Robot::random();
-        //println!("base26_alpha({}): {}", i, rnd);
-        hashes.push(rnd);
-
-        let x = i + 1; //i + 1;
-        if x % tenth == 0
-        {
-            println!("{}% : {} random numbers", 10 * (x / tenth), x); //, n / i
+            if n == 0 { break; }
         }
 
+        if chars.len() < 2
+        {
+            chars.push('A');
+        }
+
+        chars.reverse();
+        let hash: String = chars.iter().collect();
+
+        hash
     }
 
-    if has_unique_elements(hashes) == true
+    pub fn divmod(n: i32) -> (i32, i32)
     {
-        println!("HAS DUPLICATIONS");
+        let mut a = n / 26;
+        let mut b= n % 26;
+
+        if b == 26
+        {
+            a = a - 1;
+            b = b + 26;
+        }
+
+        (a, b)
     }
-    else
+
+    pub fn itoa(n: i32) -> char
     {
-        println!("NO DUPLICATIONS")
+        let c = (n + 65) as u8 as char;
+
+        c
+    }
+
+    pub fn left(s: String, n: usize) -> String
+    {
+        let len = s.len();
+        let mut s_right = s;
+
+        if len < n
+        {
+            let mut dif = n - len;
+            while dif > 0
+            {
+                s_right.push('0');
+
+                dif += 1;
+            }
+        }
+
+        s_right.chars().take(n).collect::<String>()
+    }
+
+    pub fn right(s: String, n: usize) -> String
+    {
+        let len = s.len();
+        let mut s_right = s;
+
+        if len < n
+        {
+            let mut dif = n - len;
+            while dif > 0
+            {
+                s_right.push('0');
+
+                dif += 1;
+            }
+        }
+
+        let start = len - n;
+        s_right.chars().skip(start).take(len).collect::<String>()
     }
 }
+
+
 
