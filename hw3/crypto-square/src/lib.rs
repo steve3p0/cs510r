1b85901d9da790012a5a28de9188769ea3ebfbe7
steve3p0 <stevebpdx@gmail.com>
2018-08-08 08:25:55 -0700

    issues with borrowing

--- hw3/crypto-square/src/lib.rs
+++ hw3/crypto-square/src/lib.rs
@@ -5,12 +5,51 @@ extern crate regex;
 pub fn encrypt(input: &str) -> String
 {
     let s = fmt_str(input.to_string());
-    let (c, r) = get_square(&s);
+    let (c, r) = get_dimen(&s);
+
+    let square = build_square(c, r, &s);
+
+    println!("{:?}", square);
+
+    for row in square.iter()
+    {
+        println!("{}", row);
+    }
 
     unimplemented!("Encrypt {:?} using a square code", input)
 }
 
-pub fn get_square(s: &str) -> (usize, usize)
+fn build_square(c: usize, r: usize, s: &str) -> Vec<&str>
+{
+    let mut remainder = s.clone();
+
+    let mut square:Vec<&str> = Vec::new();
+
+    for _i in 0..c
+    {
+        let mut col_length = c;
+        // let mut row = "";
+
+        let mut padding = "".to_string();
+        if remainder.len() < r
+        {
+            col_length = remainder.len();
+        }
+
+        let (mut row, mut rem) = remainder.split_at(col_length);
+
+        square.push(row);
+
+        //s = remainder;
+        remainder = rem;
+    }
+
+
+    // pad the las
+    square
+}
+
+pub fn get_dimen(s: &str) -> (usize, usize)
 {
     let len = s.len();
     let c = (len as f64).sqrt().ceil() as usize;
