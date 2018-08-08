f98d70ff27d998f6bc2a82f46c96cd3e5417d2ec
steve3p0 <stevebpdx@gmail.com>
2018-08-08 02:14:30 -0700

    initial

--- hw3/protein-translation/src/lib.rs
+++ hw3/protein-translation/src/lib.rs
@@ -1,38 +1,163 @@
+use std::collections::HashMap;
+//use uuid::Uuid;
+//use std::fmt::Formatter;
+use std::fmt;
+//use std::fmt::Debug;
 
-
-//parse(make_pairs())    Result<Vec<String>, String>
-//name_for("AUG")
-//info.name_for("").is_err()
-//of_rna("UGG UGU UAU")
-
-pub struct Proteins
+//#[derive(Debug)]
+pub struct Proteins<'a>
 {
-    codon_map: Vec<(&'static str, &'static str)>
+    map: HashMap<&'a str, &'a str>,
+}
+
+pub fn parse(condon_map: Vec<(&'static str, &'static str)>) -> Proteins
+{
+    //let mut map = HashMap::new();
+    //map = condon_map.into_iter().collect();
+
+    let map = condon_map.into_iter().collect();
+
+    Proteins { map: map }
+}
+
+impl <'a>Proteins<'a>
+{
+    pub fn name_for(&self, key: &'a str) -> Result<&str, &str>
+    {
+        if self.map.contains_key(key)
+        {
+            Ok(self.map.get(key).unwrap())
+        }
+        else
+        {
+            Err("Protein name not found")
+        }
+    }
+
+    pub fn of_rna(&self, rna_sequence: &'a str) -> Result<Vec<&str>, &str>
+    {
+        //info.of_rna("AUGUUUUGG") -> Ok(vec!["methionine", "phenylalanine", "tryptophan"])
+
+        if rna_sequence.len() % 3 != 0
+        {
+            return Err("Invalid RNA sequence");
+        }
+
+
+        let mut proteins: Vec<&str> = Vec::new();
+        let mut s = rna_sequence;
+        //let mut skip = 0;
+
+        while !s.is_empty()
+        {
+            //let condon = rna_sequence.skip(skip).take(3) as &str;
+            //.chars()
+            let (condon, rest) = s.split_at(3);
+
+            let name = self.name_for(condon).unwrap();
+
+            //let name = self.name_for(condon).unwrap();
+
+            if name == "stop codon"
+            {
+                s = "";
+            }
+            else
+            {
+                proteins.push(name); // .unwrap_or(Err("you are fucked")));
+                s = rest;
+            }
+        }
+
+        if proteins.len() > 0
+        {
+            Ok(proteins)
+        }
+        else
+        {
+            Err("Dude, something's wrong")
+        }
+    }
+
+
+}
+
+//impl Debug for Proteins
+//{
+//    //fn fmt(&self, &mut f: Formatter) -> Result
+//    fn fmt(&self, f: &mut Formatter) -> fmt::Result
+//    {
+//        write!(f.buf, "Something bad happened"); // {}", self.id);
+//    }
+//}
+
+//impl<T: fmt::Debug> fmt::Debug for Array<T> {
+//    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+//        self.data[..].fmt(formatter)
+//    }
+//}
+
+impl <'a>fmt::Debug for Proteins<'a>
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        write!(f, "This is a debugger") //Mapped Proteins {}", self.map.iter().)
+    }
 }
 
 
+//let name = self.map.get(key).unwrap() as &str;
 
-impl Proteins
-{
+//.unwrap(); //    .unwrap().to_string();
 
-    // Vec<(&'static str, &'static str)>
-    pub fn parse(condon_map: Vec<(&'static str, &'static str)>) -> Result<Vec<String>, String>
-    {
+//        {
+//            Some(n) => n,
+//            None => ""
+//        };
+//
+//        println!("name: {}", name);
 
-        unimplemented!("Not implemented!")
-    }
+//let value = match map.entry(key)
 
-    pub fn name_for(codon: &str) -> Result<String, String>
-    {
-        unimplemented!("Not implemented!")
-    }
+//let values = match self.map.get(key)
+//        let name = match self.map.get(key)
+//        {
+//            //Ok(protein) => Ok(&protein.to_string()), //  Ok(protein.unwrap().to_string()),  //., // Ok(&protein.to_string()),
+//            //Ok(protein) => Ok("fuck you"), //  self.map.get(key), //   .unwrap().to_string()),
+//            //Err(e) => Result::e //Err(e) // return Err(e) // "error" // _ // Option(&"fuckyou") // 0 // () //{ Option("cunt", e) }
+//
+//            Ok(v) => *v = "fuckyou",
+//            Err(e) => *e = 0,
+//
+//            //Some(v) => v,
+//            //None => { panic!("impossiburu!"); }
+//            //None => "Not found" //Err("impossiburu!")
+//            //None => Error("Not found") //Err("impossiburu!")
+//        };
 
-    pub fn of_rna(rna_sequence: &str) -> Result<Vec<String>, String>
-    {
-        unimplemented!("Not implemented!")
-    }
+//        match self.map.get(key)
+//        {
+//            //Ok(protein) => Ok(&protein.to_string()), //  Ok(protein.unwrap().to_string()),  //., // Ok(&protein.to_string()),
+//            //Ok(protein) => Ok("fuck you"), //  self.map.get(key), //   .unwrap().to_string()),
+//            //Err(e) => Result::e //Err(e) // return Err(e) // "error" // _ // Option(&"fuckyou") // 0 // () //{ Option("cunt", e) }
+//
+//            Ok(v) => *v = "fuckyou",
+//            Err(e) => *e = 0,
+//
+//            //Some(v) => v,
+//            //None => { panic!("impossiburu!"); }
+//            //None => "Not found" //Err("impossiburu!")
+//            //None => Error("Not found") //Err("impossiburu!")
+//        }
 
-}
 
+//name
 
-
+//        let mut value = match &self.map.entry(key)
+//        {
+//            //Entry::Occupied(o) => o.into_mut(),
+//            Entry::Occupied(v) => v,
+//            Entry::Vacant => "impossiburu!"
+//            //Entry::Vacant(v) => v, //Err("Not found")
+//            //Entry::Occupied(o) => o,
+//        };
--- hw3/protein-translation/tests/proteins.rs
+++ hw3/protein-translation/tests/proteins.rs
@@ -7,35 +7,30 @@ fn test_methionine() {
 }
 
 #[test]
-#[ignore]
 fn test_cysteine_tgt() {
     let info = proteins::parse(make_pairs());
     assert_eq!(info.name_for("UGU"), Ok("cysteine"));
 }
 
 #[test]
-#[ignore]
 fn test_stop() {
     let info = proteins::parse(make_pairs());
     assert_eq!(info.name_for("UAA"), Ok("stop codon"));
 }
 
 #[test]
-#[ignore]
 fn test_valine() {
     let info = proteins::parse(make_pairs());
     assert_eq!(info.name_for("GUU"), Ok("valine"));
 }
 
 #[test]
-#[ignore]
 fn test_isoleucine() {
     let info = proteins::parse(make_pairs());
     assert_eq!(info.name_for("AUU"), Ok("isoleucine"));
 }
 
 #[test]
-#[ignore]
 fn test_arginine_name() {
     let info = proteins::parse(make_pairs());
     assert_eq!(info.name_for("CGA"), Ok("arginine"));
@@ -44,35 +39,30 @@ fn test_arginine_name() {
 }
 
 #[test]
-#[ignore]
 fn empty_is_invalid() {
     let info = proteins::parse(make_pairs());
     assert!(info.name_for("").is_err());
 }
 
 #[test]
-#[ignore]
 fn x_is_not_shorthand_so_is_invalid() {
     let info = proteins::parse(make_pairs());
     assert!(info.name_for("VWX").is_err());
 }
 
 #[test]
-#[ignore]
 fn too_short_is_invalid() {
     let info = proteins::parse(make_pairs());
     assert!(info.name_for("AU").is_err());
 }
 
 #[test]
-#[ignore]
 fn too_long_is_invalid() {
     let info = proteins::parse(make_pairs());
     assert!(info.name_for("ATTA").is_err());
 }
 
 #[test]
-#[ignore]
 fn test_translates_rna_strand_into_correct_protein() {
     let info = proteins::parse(make_pairs());
     assert_eq!(
@@ -82,7 +72,6 @@ fn test_translates_rna_strand_into_correct_protein() {
 }
 
 #[test]
-#[ignore]
 fn test_stops_translation_if_stop_codon_present() {
     let info = proteins::parse(make_pairs());
     assert_eq!(
@@ -92,7 +81,6 @@ fn test_stops_translation_if_stop_codon_present() {
 }
 
 #[test]
-#[ignore]
 fn test_stops_translation_of_longer_strand() {
     let info = proteins::parse(make_pairs());
     assert_eq!(
@@ -102,7 +90,6 @@ fn test_stops_translation_of_longer_strand() {
 }
 
 #[test]
-#[ignore]
 fn test_invalid_codons() {
     let info = proteins::parse(make_pairs());
     assert!(info.of_rna("CARROT").is_err());
